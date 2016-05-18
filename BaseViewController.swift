e60b6fb9be6ff15cb66b7879020205e1cbc938cc
ParthHBTI <kkumar@kloudrac.com>
2016-05-18 11:34:31 +0530

    MainStoryboard UIColor change

--- BaseViewController.swift
+++ BaseViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class BaseViewController: UIViewController {
+class BaseViewController: UIViewController , UITabBarControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
